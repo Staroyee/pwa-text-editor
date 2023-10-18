1a3d9b8bf5c567e2b975a31e076d3301fd7ba026
Staroyee <danmasefield@gmail.com>
2023-10-18 17:45:00 +1030

    Created database routes

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -1,5 +1,8 @@
+// Import the 'openDB' function from the 'idb' NPM package
+// 'openDB' is used to create and manage an IndexedDB database
 import { openDB } from 'idb';
 
+// 'initdb' opens the database, upgrade is used with an if statement to check if the database already contains a store called 'jate' if it does it returns a console log, and if it doesn't exist it creates the object store with the name 'jate', keyPath of 'id', and sets autoincrement to true.
 const initdb = async () =>
   openDB('jate', 1, {
     upgrade(db) {
@@ -12,10 +15,28 @@ const initdb = async () =>
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+// Method that accepts the text editor content and adds it to the 'jate' database
+export const putDb = async (content) => {
+  console.log("Update the database");
+  const textDb = await openDB("jate", 1);
+  const tx = textDb.transaction("jate", "readwrite");
+  const store = tx.objectStore("jate");
+  const request = store.put({ id: 1, value: content });
+  const result = await request;
+  console.log("Data saved to the database", result);
+};
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+// Method that gets all the text editor content from the 'jate' database
+export const getDb = async () => {
+  console.log("GET from the database");
+  const textDb = await openDB("jate", 1);
+  const tx = textDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.get(1);
+  const result = await request;
+  console.log("result.value", result);
+  return result?.value;
+};
 
+// Call initdb function to initialise the database when the module is loaded.
 initdb();
